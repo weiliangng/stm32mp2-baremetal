89f570ec2aed97f51366c7ea06a15c230ab0420d
weiliangng <46848740+weiliangng@users.noreply.github.com>
2025-10-03 17:51:59 +0800

    added bash script

---
+++ minimal_boot/build_userfs_image.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+MINIMAL_BOOT_DIR="${DIR:-$HOME/stm32mp2-baremetal/minimal_boot}"
+STAGE_DIR="${MINIMAL_BOOT_DIR%/}/../ext/userfs_stage"
+OUTPUT_IMAGE="${MINIMAL_BOOT_DIR%/}/../ext/st-image-userfs-openstlinux-weston-stm32mp2.userfs.ext4"
+
+cd "$MINIMAL_BOOT_DIR"
+make
+mkimage -A arm64 -O u-boot -T standalone -C none \
+  -a 0x88000000 -e 0x88000000 \
+  -n "stm32mp2-baremetal" \
+  -d build/main.bin build/main-standalone.uimg
+
+mkdir -p "$STAGE_DIR"
+install -m 0644 build/main-standalone.uimg "$STAGE_DIR/main-standalone.uimg"
+rm -f "$OUTPUT_IMAGE"
+mkfs.ext4 -q -d "$STAGE_DIR" -b 4096 -L userfs "$OUTPUT_IMAGE" 4096
