fff4b5e65493a1db50adce02f21834e623212825
weiliangng <46848740+weiliangng@users.noreply.github.com>
2025-10-13 10:48:09 +0800

    echo script

--- minimal_boot/main.c
+++ minimal_boot/main.c
@@ -1,11 +1,21 @@
 #include <stdint.h>
+
 #define USART_BASE  0x400E0000u
 #define USART_ISR   (*(volatile uint32_t *)(USART_BASE + 0x1Cu))
+#define USART_RDR   (*(volatile uint8_t  *)(USART_BASE + 0x24u))
 #define USART_TDR   (*(volatile uint8_t  *)(USART_BASE + 0x28u))
-#define TXE_BIT     (1u << 7)
+
+#define RXNE_BIT    (1u << 5)   // RX data ready
+#define TXE_BIT     (1u << 7)   // TX register empty
 
 void main(void) {
-    while ((USART_ISR & TXE_BIT) == 0) { }
-    USART_TDR = (uint8_t)'A';
-    for (;;) __asm__ volatile("wfe");
+    for (;;) {
+        // wait for a byte
+        while ((USART_ISR & RXNE_BIT) == 0) { }
+        uint8_t c = USART_RDR;          // reading clears RXNE
+
+        // echo it
+        while ((USART_ISR & TXE_BIT) == 0) { }
+        USART_TDR = c;
+    }
 }
